4eed0194c4072d849de0c34d8a6ba46bf3f9a511
eyosidesta <mamesmsdesta@gmail.com>
2024-01-31 05:23:23 -0500

    query man for test

--- SolveMysteryMurder.py
+++ SolveMysteryMurder.py
@@ -1,3 +1,8 @@
 import pytholog as pl
 
 set_nb = pl.knowledge_base("murderer")
+set_nb(["man(dr_black)",
+        "man(reverend_green)",
+        "man(miss_scarlett)",
+        ])
+print(set_nb.query(pl.Expr("man(dr_black)")))
